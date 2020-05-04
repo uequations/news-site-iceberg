b0f9b82b3d349824c25e675496133c5a90fb4151
malkebu-lan <malkebu-lan@uequations.com>
2020-05-04 15:53:36 -0400

    removed pt.json

--- config/i18n.js
+++ config/i18n.js
@@ -8,8 +8,8 @@ module.exports = {
     dateFormat: `MM/DD/YYYY`,
     siteLanguage: `en`,
     ogLanguage: `en_US`,
-    defaultTitle: `Gatsby Starter with multi-language and CMS`,
-    defaultDescription: `Gatsby example site using Markdown, i18n and CMS`,
+    defaultTitle: `IRS NEWS | Universal Equations`,
+    defaultDescription: `News from various communities, government agencies, and organizations.`,
   },
   irs: {
     path: `irs`,
@@ -17,16 +17,7 @@ module.exports = {
     dateFormat: `MM/DD/YYYY`,
     siteLanguage: `en`,
     ogLanguage: `en_US`,
-    defaultTitle: `IRS NEWS | Universal Equations`,
+    defaultTitle: `NEWS | Universal Equations`,
     defaultDescription: `The Internal Revenue Service is the nation's tax collection agency and administers the Internal Revenue Code enacted by Congress.`,
-  },
-  pt: {
-    path: `pt`,
-    locale: `pt-BR`,
-    dateFormat: `DD/MM/YYYY`,
-    siteLanguage: `pt`,
-    ogLanguage: `pt_BR`,
-    defaultTitle: `Gatsby Starter multilingue com CMS`,
-    defaultDescription: `Exemplo de Gatsby site usando Markdown, i18n e CMS`,
   },
 }
